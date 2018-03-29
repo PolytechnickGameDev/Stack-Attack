3fad28b8c51b61eaabdbd6dbdf3f4a84a7d6925b
Nazarkon <nazarmontsibovych@gmail.com>
2018-03-30 00:28:07 +0300

    v0.0.0.4 add Hero move

---
+++ Assets/HeroMove.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroMove : MonoBehaviour
+{
+
+    public float speed;
+    public float jump;
+
+    private float gravityForce;
+    private Vector3 moveVector;
+
+    private CharacterController ch_controller;
+    void Start()
+    {
+        ch_controller = GetComponent<CharacterController>();
+
+    }
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        moveVector = Vector3.zero;
+        moveVector.x = Input.GetAxis("Horizontal") * speed;
+
+        ch_controller.Move(moveVector * Time.deltaTime);
+    }
+
+}
