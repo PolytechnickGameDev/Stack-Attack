75bf34bae01886f48468f7244fad73cac4ad3748
Mr-Computer <sashacomputer399@gmail.com>
2018-04-17 16:50:58 +0300

    v0.0.1.3 add fisick script

---
+++ Assets/Scripts/Fithick.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fithick : MonoBehaviour {
+
+    public GameObject _object;
+
+
+    // Use this for initialization
+    void Start () {
+        _object = (GameObject)this.gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(!(_object))
+        _object.transform.position -= Vector3.up * Time.deltaTime;
+        
+	}
+
+    void OnCollisionEnter()
+    {
+        Debug.Log("Hit Something");  // Передаем сообщение в консоль Unity  
+    }
+}
